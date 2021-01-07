2104141563efea51b29983ce958ffcaac38d55c8
Eir <diianaapop@gmail.com>
2021-01-07 11:28:32 +0100

    Mise en place

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # verytiny3d
 3D Render with Tiny Renderer lessons
+
+Tiny Renderer by ssloy : https://github.com/ssloy/tinyrenderer
+Files imported from Tiny Renderer : tgaimage.cpp, tgaimage.h, CMakeLists.txt
+
+To compile :
+``` sh
+mkdir build &&
+cd build &&
+cmake .. &&
+make &&
+./tinyrenderer
+```
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "tgaimage.h"
+
+constexpr int width = 100;
+constexpr int height = 100;
+
+const TGAColor white = { 255, 255, 255, 255 };
+
+
+int main(){
+  TGAImage image(width, height, TGAImage::RGB);
+  image.set(30, 30, white); // dessine un gentil petit pixel blanc
+  //line(13, 20, 80, 40, image, white); // dessine une ligne
+  image.write_tga_file("framebuffer.tga");
+  return 0;
+}
